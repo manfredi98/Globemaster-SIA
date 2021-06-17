28f73a36f855946d597bedec8012769ba1515370
manfredi98 <lmanfredib@gmail.com>
2021-06-17 10:12:39 -0400

    3.0

--- globemaster/urls.py
+++ globemaster/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import *
 
 from totus.views import *
 
@@ -28,5 +28,6 @@ urlpatterns = [
     path('clientes', clientes),
     path('dashboard', dashboard),
 
-    path('accounts/', include('django.contrib.auth.urls')),
+path('accounts/', include('django.contrib.auth.urls'))
+
 ]
